073ab964220173f4daf79157d12a7f1d43c5963a
Mac Yeh <81870113+paipaiqi167@users.noreply.github.com>
2021-09-10 15:02:00 +0800

    Update ParentDAG.R

--- ParentDAG.R
+++ ParentDAG.R
@@ -32,42 +32,76 @@ ParentDAG <- function(x.pos=NA,graphEst){
 
 }
 
-################## S: the vector of exposure
+################## S: the matrix of exposure
 ################## G: the matrix of all the mediator
 ################## Y: the vector of outcome
+################## s: the exposure node you may concern
 ################## g: the mediator node you may concern
 
-
-INDAG <- function(S,G,Y,g,Confound=NA){
+INDAG <- function(S,G,Y,s,g,Confound=NA){
   if(is.na(Confound))
   {
-    beta_part1 = coef(lm(G~S))[2]
+    p = dim(S)[2]
+    q = dim(G)[2]
+    n = dim(S)[1]
+    
+    s.G = apply(G, 2, function(x)x=x-mean(x))
+    s.S = S - mean(S)
+    s.Y = Y - mean(Y)
+    
+    Part1 = lm(G[,g]~S[,s])
+    beta_part1 = coef(Part1)[2]
+ 
+    R = s.G[,g] - s.S[,g]*coef(Part1)[2]
+    Z_part1 = s.S[,g]*R
+   
     suffStat <- list(C = cor(G), n = nrow(G))
     CP =  pc(suffStat, indepTest = gaussCItest,
                 p = ncol(G), alpha = 0.01)
-    
     Pr = ParentDAG(g,CP@graph)
     
     beta_part2 = 0
+    Z_part2 = 0
     for(i in c(1:Pr$ndag))
     {
       if(Pr$pnode[[i]][1]!=0)
+      {
         M = lm(Y~G[,g]+G[,c(Pr$pnode[[i]])])
-      
-      beta_part2 = 0
-      beta_part2 = beta_part2 + coef(M)[2]
-      
+        beta_part2 = beta_part2 + coef(M)[2]
+        
+        R = Y - s.G[,c(g,Pr$pnode[[i]])]%*%M$coefficients[-1]
+        Z_part2 = Z_part2 + ( t(solve(cov(s.G[,c(g,Pr$pnode[[i]])]))%*%t(s.G[,c(g,Pr$pnode[[i]])]))[,1]*R )
+          
+      }else{
+        
+        M = lm(Y~G[,g])
+        beta_part2 = beta_part2 + coef(M)[2]
+        
+        R = Y - s.G[,g]*M$coefficients[-1]
+        Z_part2 = Z_part2 + (s.G[,g]*R)
+        
+      }
     }
+    
     beta_part2 = beta_part2/Pr$ndag
+    Z_part2 = as.vector(Z_part2/Pr$ndag)
+    point_estimate = as.numeric(beta_part1*beta_part2)
+    
+    standard = sqrt(mean( (beta_part2*Z_part2 + beta_part1*Z_part1)^2))
+    
+    Quantity = sqrt(n)*point_estimate/standard
+    p.value = 2*(1-pnorm(abs(Quantity)))
     
     IND = list()
-    IND$indeffect = as.numeric(beta_part1*beta_part2)
+    IND$indeffect = point_estimate
+    IND$Statistics = Quantity
+    IND$p.value = p.value
+    IND$upper = point_estimate + 1.96*standard/sqrt(n)
+    IND$lower = point_estimate - 1.96*standard/sqrt(n)
     
     return(IND)
     
-    
   }else{
-    
     beta_part1 = coef(lm(G~S+Confound))[2]
     suffStat <- list(C = cor(G), n = nrow(G))
     CP =  pc(suffStat, indepTest = gaussCItest,
@@ -89,13 +123,8 @@ INDAG <- function(S,G,Y,g,Confound=NA){
     
     IND = list()
     IND$indeffect = as.numeric(beta_part1*beta_part2)
-    
+   
     return(IND)
-    
-    
-    
-    
-    
+   
   }
-  
 }
