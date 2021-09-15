74b8bfb286d7b41e4a79256e71662a3de8df122f
Mac Yeh <81870113+paipaiqi167@users.noreply.github.com>
2021-09-15 21:14:13 +0800

    Update Data generating.R

--- Data generating.R
+++ Data generating.R
@@ -45,3 +45,24 @@ for(i in c(1:n))
 }
 
 X = cbind(Exposure,Mediator,Y)
+
+
+M_generate <- function(q,p){
+  M_matrix = diag(rep(1,q),)
+  for(i in c(1:(q-1)))
+  {
+    for(j in c((i+1):q))
+    {
+      if(runif(1,0,1) <= p)
+      {
+        if(runif(1,0,1) <= 0.5)
+        {
+          M_matrix[i,j] = ifelse(runif(1,0,1) <= 0.5, 1,-1)*runif(1,0.5,2)
+        }else{
+          M_matrix[j,i] = ifelse(runif(1,0,1) <= 0.5, 1,-1)*runif(1,0.5,2)
+        }
+      }
+    }
+  }
+  return(M_matrix)
+}
